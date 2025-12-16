9dea44914f713c1f2a9cfa01e2084071686caafa
Jonathan Su <jonathansu@tenstorrent.com>
2025-12-16 00:22:18 +0000

    remove tiles per chunk, unused

--- ttnn/cpp/ttnn/operations/experimental/ccl/strided_all_gather_minimal_matmul_async/device/strided_all_gather_minimal_matmul_async_op.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/strided_all_gather_minimal_matmul_async/device/strided_all_gather_minimal_matmul_async_op.cpp
@@ -130,7 +130,6 @@ ttnn::operations::experimental::ccl::strided_all_gather_minimal_matmul_async::St
                 topology,
                 multi_device_global_semaphore,
                 cluster_axis,
-                /*tiles_per_chunk=*/std::nullopt,
                 num_workers_per_link,
                 num_buffers_per_channel,
                 config->compute_with_storage_grid_size.y,
