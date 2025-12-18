24e00c9a2721958b33f012951365689caab2000d
John Bauman <jbauman@tenstorrent.com>
2025-12-18 22:01:50 +0000

    improve

--- tt_metal/impl/device/device_manager.cpp
+++ tt_metal/impl/device/device_manager.cpp
@@ -485,7 +485,6 @@ void DeviceManager::initialize_active_devices() {
             continue;
         }
         events.emplace_back(detail::async([&, dev, mmio_device_id]() {
-
             auto tunnels_from_mmio =
                 tt::tt_metal::MetalContext::instance().get_cluster().get_tunnels_from_mmio_device(mmio_device_id);
             dev->init_command_queue_device();
@@ -501,7 +500,7 @@ void DeviceManager::initialize_active_devices() {
                     }
                 }
             }
-        );
+        }));
     }
     for (const auto& event : events) {
         event.get();
